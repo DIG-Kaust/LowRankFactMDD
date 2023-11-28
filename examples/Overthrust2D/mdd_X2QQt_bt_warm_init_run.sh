63764f748b1af640ca0bf2bc6d1d8d76ab1cd6ba
i5fchen <105012768+i5fchen@users.noreply.github.com>
2023-11-28 14:12:25 +0300

    Update mdd_X2QQt_bt_warm_init_run.sh

--- examples/Overthrust2D/mdd_X2QQt_bt_warm_init_run.sh
+++ examples/Overthrust2D/mdd_X2QQt_bt_warm_init_run.sh
@@ -1,1 +1,20 @@
 
+#!/bin/bash
+##SBATCH --nodes 2
+#SBATCH --partition=batch
+#SBATCH -J mdd_xlr
+#SBATCH -o mdd_xlr.%J.out
+##SBATCH --time=320:00
+#SBATCH --time=150:00
+#SBATCH --mem=50G
+
+#SBATCH --ntasks=16
+#SBATCH --cpus-per-task=16
+
+#SBATCH --constraint=amd
+module loading python/3.7.0
+module loading mpi4py/3.0.3-py3.7.0-ompi4.0.3
+
+# module loading anaconda3/2019.10
+
+mpiexec -n 16 python3 mdd_fx_X2QQt_bt_warm_init.py -fstart=0 -fend=560 -nit=200000 -tol=1e-6 -nr=100 -rgQ=0 -lam=0.9 -inertia=0.0 
