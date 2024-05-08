f1fd88091e7234dc5ba3d3503aaa83eeba80998c
FuqiangChen <foobar.fuqiang.chen@gmail.com>
2024-05-08 13:37:30 +0300

    fixed bugs with alpha

--- mdd_lrr/utils.py
+++ mdd_lrr/utils.py
@@ -1,42 +1,37 @@
 import numpy as np
 
-class alpha_type1:
-    t0 = 0
-    def __init__(self):
-        self.t0 = alpha_type1.t0
-    def pop(self):
-        alpha = self.t0/(self.t0+3)
-        self.t0 += 1
-        return alpha
-
-class alpha_type2:
-    t0 = 1
-    def __init__(self):
-        self.t0 = alpha_type2.t0
-    def pop(self):
-        t1 = 0.5*(1+np.sqrt(1+4*self.t0**2))
-        alpha = (self.t0-1.0) / t1
-        self.t0 = t1
-        return alpha
-
 def ipalm(A, b, prox, tau, alpha, tol=1e-5, scale=[0.5, 0.5], lam=[0.1,0.1], iter_lim=None, show=False, x0=None, beta=None):
 
-    gnorm0 = np.inf
 
     x1 = list(x0)
     x2 = list(x0)
     tau = list(tau)
+    
+    if lam is None:
+        lam=[0.1]*len(x1)
+
+    if 0 == alpha:
+        alpha = [i/(i+3.0) for i in range(iter_lim)]
+
+    if 1 == alpha:
+        alpha = []
+        t0 = 1
+        for i in range(iter_lim):
+            t1 = 0.5*(1+np.sqrt(1+4*t0**2))
+            alpha.append((t0-1)/t1)
+            t0 = t1
 
     if show:
-            print("%5s%20s%20s%20s\n"%("niter", "data_residual", "norm_grad", "rel_grad_diff"))
+            print("\n%5s%20s%20s%20s\n"%("niter", "data_residual", "norm_grad", "rel_grad_diff"))
 
+    total_misfit = 0
     for it in range(0, iter_lim):
 
-        gnorm = 0
 
+        total_misfit_new = total_misfit = 0
         for iblck in range(len(x0)):
 
-            yiblck = x1[iblck] + alpha.pop() * (x1[iblck] - x0[iblck])
+            yiblck = x1[iblck] + alpha[it] * (x1[iblck] - x0[iblck])
 
             if beta is not None:
                 ziblck = x1[iblck] + beta * (x1[iblck] - x0[iblck])
@@ -48,7 +43,8 @@ def ipalm(A, b, prox, tau, alpha, tol=1e-5, scale=[0.5, 0.5], lam=[0.1,0.1], ite
             res = A.forward(x2) - b
             f0 = 0.5*np.linalg.norm(res)**2
             grad = A.adjoint(res, x2, iblck)
-            gnorm += np.linalg.norm(grad)
+            # gnorm += np.linalg.norm(grad)
+            total_misfit += f0
 
             while(True):
 
@@ -60,22 +56,25 @@ def ipalm(A, b, prox, tau, alpha, tol=1e-5, scale=[0.5, 0.5], lam=[0.1,0.1], ite
                 f1 = 0.5*np.linalg.norm(res)**2
 
                 if backtrack(f1-f0, x1_tmp-yiblck, grad, tau[iblck]) or tau[iblck]<1e-12:
+                    total_misfit_new += f1
                     break
                 else:
                     tau[iblck] *= scale[iblck]
+
 
         x0 = x1
         x1 = x2
 
         if show:
-            print("%5d%20.3f%20.3f%20.6f"%(it, np.linalg.norm(res), np.linalg.norm(grad), np.abs(gnorm/gnorm0-1.0)))
+            print("%5d%20.3f%20.3f%20.6f"%(it, np.linalg.norm(res), np.linalg.norm(grad), np.abs(total_misfit/total_misfit_new-1.0)))
 
-        if np.abs(gnorm/gnorm0-1.0)<tol:
+        if np.abs(total_misfit_new/total_misfit-1.0)<tol:
             break
 
-        gnorm0 = gnorm
+        # gnorm0 = gnorm
 
     return x2
+
 
 def l2_prox(v, t):
 
